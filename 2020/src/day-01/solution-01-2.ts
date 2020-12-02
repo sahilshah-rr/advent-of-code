da7896807dc8093484d8fc9725c6ef7d04255743
Sahil Shah <sahilshah.rr@gmail.com>
2020-12-02 18:15:52 -0500

    Solve day 1 challenge 2

---
+++ 2020/src/day-01/solution-01-2.ts
@@ -0,0 +1,20 @@
+import { readLines } from '../util'
+
+async function solve (): Promise<void> {
+  const target = 2020
+  const numbers = new Set<number>()
+  await readLines('src/day-01/input.txt', (line: string) => {
+    const number = Number(line)
+    const remainder = target - number
+    for (const left of numbers) {
+      const right = remainder - left
+      if (left !== right && numbers.has(right)) {
+        console.log(number * left * right)
+        return
+      }
+    }
+    numbers.add(number)
+  })
+}
+
+void solve()
