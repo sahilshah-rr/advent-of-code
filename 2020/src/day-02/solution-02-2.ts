89a24b5a9465cdf843e7455a9686490fa248388e
Sahil Shah <sahilshah.rr@gmail.com>
2020-12-02 18:16:26 -0500

    Solve day 2 challenge 2

---
+++ 2020/src/day-02/solution-02-2.ts
@@ -0,0 +1,47 @@
+import { readLines } from '../util'
+
+interface ParsedLine {
+  first: number
+  second: number
+  char: string
+  password: string
+}
+
+function passwordValid ({ first, second, char, password }: ParsedLine): boolean {
+  let count = 0
+  if (password[first - 1] === char) {
+    count++
+  }
+  if (password[second - 1] === char) {
+    count++
+  }
+  return count === 1
+}
+
+function parseLine (line: string): ParsedLine | null {
+  const lineFormat = /^(?<first>\d+)-(?<second>\d+)\s(?<char>[a-zA-Z]):\s(?<password>[a-zA-Z]+)$/
+  const parts = line.match(lineFormat)?.groups
+  if (parts !== undefined) {
+    return {
+      first: Number(parts.first),
+      second: Number(parts.second),
+      char: String(parts.char),
+      password: String(parts.password)
+    }
+  } else {
+    return null
+  }
+}
+
+async function solve (): Promise<void> {
+  let count = 0
+  await readLines('src/day-02/input.txt', (line: string) => {
+    const parsedLine = parseLine(line)
+    if (parsedLine !== null && passwordValid(parsedLine)) {
+      count++
+    }
+  })
+  console.log(count)
+}
+
+void solve()
