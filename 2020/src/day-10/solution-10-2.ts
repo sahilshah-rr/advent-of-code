563eb478adcd22edb35d6fd6adf94d4371206b0e
Sahil Shah <sshah@better.com>
2020-12-15 13:17:43 -0500

    Optimize day 10 challenge 2

--- 2020/src/day-10/solution-10-2.ts
+++ 2020/src/day-10/solution-10-2.ts
@@ -1,31 +1,30 @@
 import { readLines, syncSolve } from '../util'
 
-function combo (adapters: number[], index: number, cache: Map<number, number>): number {
-  const jolt = adapters[index] as number
-  if (index === 0) {
-    return 1
-  } else if (cache.has(jolt)) {
-    return cache.get(jolt) as number
-  } else {
-    let count = 0
-    for (let i = index - 1; i >= index - 3 && i >= 0; i--) {
-      if ((jolt - (adapters[i] as number)) <= 3) {
-        count += combo(adapters, i, cache)
-      }
-    }
-    cache.set(jolt, count)
-    return count
-  }
-}
-
 async function solve (): Promise<void> {
-  const adapters: number[] = [0]
+  const adapters = new Set<number>()
+  let target = 0
   await readLines('src/day-10/input.txt', (line) => {
-    adapters.push(Number(line))
+    const adapter = Number(line)
+    adapters.add(adapter)
+    if (adapter > target) {
+      target = adapter
+    }
   })
-  adapters.sort((a, b) => a - b)
-  adapters.push(adapters[adapters.length - 1] as number + 3)
-  console.log(combo(adapters, adapters.length - 1, new Map<number, number>()))
+  target += 3
+  adapters.add(target)
+  const counts = Array(target + 1).fill(0)
+  counts[0] = 1
+  for (let i = 1; i <= target; i++) {
+    if (adapters.has(i)) {
+      let count = 0
+      for (let j = i - 1; j >= i - 3 && j >= 0; j--) {
+        count += (counts[j] as number)
+      }
+      counts[i] = count
+    }
+  }
+  console.log(counts)
+  console.log(counts[counts.length - 1])
 }
 
 syncSolve(solve)
