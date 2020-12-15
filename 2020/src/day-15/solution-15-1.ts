b3040fb6b9a87af366b59b6836c49269451a0b32
Sahil Shah <sshah@better.com>
2020-12-15 00:17:29 -0500

    Solve day 15 challenge 1

---
+++ 2020/src/day-15/solution-15-1.ts
@@ -0,0 +1,28 @@
+import { readFile, syncSolve } from '../util'
+
+async function solve (): Promise<void> {
+  const numbers = new Map<number, number[]>()
+  const line = await readFile('src/day-15/input.txt')
+  let lastNum = -1
+  line.split(',').forEach((num, index) => {
+    lastNum = Number(num)
+    numbers.set(lastNum, [index])
+  })
+  for (let i = numbers.size; i < 2020; i++) {
+    const prev = numbers.get(lastNum) as number[]
+    if (prev.length === 1) {
+      lastNum = 0
+    } else {
+      lastNum = (prev[prev.length - 1] as number) - (prev[prev.length - 2] as number)
+    }
+    let nextPrev = numbers.get(lastNum)
+    if (nextPrev === undefined) {
+      nextPrev = []
+    }
+    nextPrev.push(i)
+    numbers.set(lastNum, nextPrev)
+  }
+  console.log(lastNum)
+}
+
+syncSolve(solve)
