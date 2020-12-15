1b1624fa4f080319aed451120a18678a42e99424
Sahil Shah <sshah@better.com>
2020-12-15 11:20:26 -0500

    Optimize day 15 challenge 2

--- 2020/src/day-15/solution-15-2.ts
+++ 2020/src/day-15/solution-15-2.ts
@@ -1,31 +1,20 @@
 import { readFile, syncSolve } from '../util'
 
 async function solve (): Promise<void> {
-  const numbers = new Map<number, number[]>()
-  const line = await readFile('src/day-15/input.txt')
-  let lastNum = -1
-  line.split(',').forEach((num, index) => {
-    lastNum = Number(num)
-    numbers.set(lastNum, [index])
-  })
-  for (let i = numbers.size; i < 30000000; i++) {
-    const prev = numbers.get(lastNum) as number[]
-    if (prev.length === 1) {
-      lastNum = 0
-    } else {
-      lastNum = (prev[prev.length - 1] as number) - (prev[prev.length - 2] as number)
-    }
-    let nextPrev = numbers.get(lastNum)
-    if (nextPrev === undefined) {
-      nextPrev = []
-    }
-    nextPrev.push(i)
-    if (nextPrev.length > 2) {
-      nextPrev.shift()
-    }
-    numbers.set(lastNum, nextPrev)
+  const numbers = new Map<number, number>()
+  const starters = (await readFile('src/day-15/input.txt')).split(',')
+  let prev = -1
+  let i = 0
+  for (; i < starters.length; i++) {
+    prev = Number(starters[i])
+    numbers.set(prev, i)
   }
-  console.log(lastNum)
+  for (; i < 30000000; i++) {
+    const prevIndex = numbers.get(prev) as number
+    numbers.set(prev, i - 1)
+    prev = prevIndex === undefined ? 0 : i - 1 - prevIndex
+  }
+  console.log(prev)
 }
 
 syncSolve(solve)
