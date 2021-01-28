34766791265c4aae9eb9dfee5866dd0497b11c60
Sahil Shah <sshah@better.com>
2021-01-27 21:17:03 -0500

    Unfinished day 1

--- 2016/src/day-01/day-01-solution-2.rb
+++ 2016/src/day-01/day-01-solution-2.rb
@@ -1,5 +1,11 @@
+require 'set'
+set = Set.new([0+0i])
 puts File
   .read("#{__dir__}/input.txt").strip.split(', ') # Read input
   .map { |i| [i[0] == 'R' ? -1i : +1i, i[1..-1].to_i] } # Translate instructions
-  .reduce([0+0i, 0+1i]) { |(p, d), (i, s)| [p += (d *= i) * s, d] } # Move self
+  .reduce([0+0i, 0+1i]) { |(p, d), (i, s)|
+    d *= i
+    s.times { set.add(p += d) unless set.include?(p + d) }
+    [p, d]
+  } # Move self
   .first.rect.map(&:abs).sum # Calculate distance
